7e493b550cacb01111723597ecb64ef73cbdbcf8
RizzieRu <rizzie@mail.ru>
2024-12-03 08:29:31 +0300

    Добавьте файлы проекта.

---
+++ SFModuleFive/Program.cs
@@ -0,0 +1,26 @@
+﻿void Echo(string text, int deep)
+{
+	string modif = text;
+
+	if (modif.Length > 2)
+	{
+		modif = modif.Remove(0, 2);
+		Console.BackgroundColor = (ConsoleColor) deep;
+		Console.WriteLine($"...{modif}");
+	}
+
+	if (deep > 1)
+	{
+		Echo(modif, deep - 1);
+	}
+}
+
+Console.WriteLine("Введите текст:");
+
+string text = Console.ReadLine();
+
+Console.WriteLine("Введите глубину эха:");
+
+int deep = Convert.ToInt32(Console.ReadLine());
+
+Echo(text, deep);
